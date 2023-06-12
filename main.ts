ceb9a31227b25d9fc73307814294e6c60d7b63f3
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-06-12 11:20:01 +1000

    v0.0.0 --version Flag
    Able to print the version to the console when doing `deno run main.ts --version`

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log('Hello World')
+import { Command, FlagBoolean, Parser } from "https://deno.land/x/flags@0.0.3/mod.ts"
+
+new Parser(new Command({
+	use: 'main.ts',
+	short: 'Merge several log files and zipped them up.',
+	long: 'Merge several log files into one and zipped up. Logs are all sorted alphabetically.',
+	prepare(flags) {
+		flags.bool({
+			name: 'version',
+			short: 'v',
+			usage: 'Print app\'s version'
+		})
+		return function (_args) {
+			if ((flags.find('version') as FlagBoolean).value)
+				return console.log('Version: v0.0.0')
+		}
+	}
+})).parse(Deno.args)
